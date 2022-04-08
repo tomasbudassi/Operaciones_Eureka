8266a8477106daec1d9e1419acba3b584a1295ff
Tomas <tomasbudassi@gmail.com>
2022-04-08 16:40:15 -0300

    fix 1

--- src/main/java/uda/operaciones/controllers/OperacionController.java
+++ src/main/java/uda/operaciones/controllers/OperacionController.java
@@ -6,25 +6,37 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import uda.operaciones.clientes.ResultadoClienteRest;
-
 @RestController
 public class OperacionController {
 
 	@Autowired
 	private RestTemplate clienteRest;
 	
-	@Autowired
-	private ResultadoClienteRest clienteFeign;
-	
-	@GetMapping("/sumar")
-	public String sumar(@RequestParam float a, @RequestParam float b) {
-		String resultado = Float.toString(a+b);
-		return clienteRest.getForObject("http://resultados/resultado/" + resultado, String.class);
+	@GetMapping("/hello")
+	public String saludo() {
+		return "Hola mundo";
 	}
-	
-	@GetMapping("/restar")
+
+	@GetMapping("/sumar_eeuu")
+	public String sumar(@RequestParam float a, @RequestParam float b) {
+		String resultado = Float.toString(a + b);
+		return clienteRest.getForObject("https://resultados-usa-tbudassi.herokuapp.com/resultado/" + resultado , String.class);
+	}
+
+	@GetMapping("/restar_eeuu")
 	public String restar(@RequestParam float a, @RequestParam float b) {
-		return clienteFeign.mostrarResultado(a-b);
-	}	
+		String resultado = Float.toString(a - b);
+		return clienteRest.getForObject("https://resultados-usa-tbudassi.herokuapp.com/resultado/" + resultado , String.class);
+	}
+
+	@GetMapping("/sumar_europa")
+	public String sumarEuropa(@RequestParam float a, @RequestParam float b) {
+		String resultado = Float.toString(a + b);
+		return clienteRest.getForObject("https://resultados-europa-tbudassi.herokuapp.com/resultado/" + resultado , String.class);
+	}
+	@GetMapping("/restar_europa")
+	public String restarEuropa(@RequestParam float a, @RequestParam float b) {
+		String resultado = Float.toString(a - b);
+		return clienteRest.getForObject("https://resultados-europa-tbudassi.herokuapp.com/resultado/" + resultado , String.class);
+	}
 }
