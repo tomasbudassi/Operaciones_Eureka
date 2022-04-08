95873ec5b23dba2c57c932d17d8fe257b31e95d3
Tomas <tomasbudassi@gmail.com>
2022-04-08 17:01:20 -0300

    fix 2

--- src/main/java/uda/operaciones/OperacionesApplication.java
+++ src/main/java/uda/operaciones/OperacionesApplication.java
@@ -3,9 +3,7 @@ package uda.operaciones;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
 @EnableEurekaClient
 public class OperacionesApplication {
--- src/main/java/uda/operaciones/RestTemplateConfig.java
+++ src/main/java/uda/operaciones/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package uda.operaciones;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 	
 	@Bean("clienteRest")
-	@LoadBalanced
 	public RestTemplate registrarRestTemplate() {
 		return new RestTemplate();
 	}
